6f26ad3cb1a9e425cc987b2ef2beead66f41a651
bforks <brittanyforks@gmail.com>
2014-07-16 18:03:08 -0700

    Commenting out tweeted module

--- work/sidebar.php
+++ work/sidebar.php
@@ -1,23 +1,23 @@
 <div class="sidebar">
+  <section class="feed dribbble">
+     <h1>Dribbblin'</h1>
+     <ul></ul>
+  </section>
 
-   <section class="feed dribbble">
-       <h1>Dribbblin'</h1>
-       <ul></ul>
-   </section>
+  <!--
+  <section class="feed twitter">
+     <h1>Tweeted</h1>
+     <ul class="linkify"></ul>
+  </section>
+  -->
 
-   <section class="feed twitter">
-       <h1>Tweeted</h1>
-       <ul class="linkify"></ul>
-   </section>
-
-	<section class="social icons">
-       <h1>Elsewhere</h1>
-       <ul>
-       		<li class="pinterest"><a href="http://pinterest.com/beeforks/">Pinterest</a></li>
-       		<li class="ffffound"><a href="http://ffffound.com/home/brittany/recommend/">ffffound</a></li>
-       		<li class="forrst"><a href="http://forrst.com/people/brittanyforks/posts">Forrst</a></li>
-			    <li class="lastfm"><a href="http://www.last.fm/user/omybritters">Last.fm</a></li>
-       </ul>
-   </section>
-
+  <section class="social icons">
+     <h1>Elsewhere</h1>
+     <ul>
+        <li class="pinterest"><a href="http://pinterest.com/beeforks/">Pinterest</a></li>
+        <li class="ffffound"><a href="http://ffffound.com/home/brittany/recommend/">ffffound</a></li>
+        <li class="forrst"><a href="http://forrst.com/people/brittanyforks/posts">Forrst</a></li>
+        <li class="lastfm"><a href="http://www.last.fm/user/omybritters">Last.fm</a></li>
+     </ul>
+  </section>
 </div>

ff14cf9d66ce6a6162bbebf5502bb5401e31f1f3
bforks <brittanyforks@gmail.com>
2014-08-05 16:10:20 -0700

    Adding Kudos page

---
+++ work/kudos.php
@@ -0,0 +1,35 @@
+<?php include_once 'top.php'; ?>
+<div class="main-with-sidebar">
+	<img class="retina" src="../img/work/kudos-identity.png" />
+	<div class="description-text" style="clear: both;">
+		<h1>Kiip Kudos</h1>
+		<p>Kudos is a iOS/Android app that rewards you for your social media activity on Facebook, Twitter, Instagram, Linkedin. For example  I inherited this app from Kiip's former designer, <a href="http://www.heyadam.com/" target="_blank">Adam Debreczeni</a>. Adam did an excellent job setting up the foundation for Kudos. My main focus was transitioning Kudos from iOS 6 to an iOS 7 based design. I also created the onboarding flow and overall branding for the app. </p>
+	</div>
+
+	<img class="retina" src="../img/work/kudos-onboarding.png" />
+	<div class="description-text" style="clear: both;">
+		<p>Since Kudos is a new product, one of the main objectives of the onboarding is to educate users on the purpose of the app. I designed the onboarding to be as lightweight and concise as possible.</p>
+	</div>
+
+	<img class="retina" src="../img/work/kudos-pages.png" />
+	<div class="description-text" style="clear: both;">
+		<p>The interior pages of kudos were designed to take design cues from the iOS 7 to create a familiar environment.</p>
+		<p>The welcome screen is to give users some clues about actions they can take to recieve rewards.</p>
+		<p>When you achieve an reward you'll be shown this screen. I wanted to promently display the reward along with detailed steps on how you can redeem your reward. My main goal was to make redeeming rewards as easy as possible.</p>
+		<p>The reward detail screen gives you details about what you did to achieve your reward. Your avatar along with the action you preformed is placed promenently</p>
+		<p>The last screen shows the Kudos reward feed. This is the central view in the app. This shows gives you a quick view of the rewards you've earned and how you earned them.</p>
+		</div>
+		<img class="retina" src="../img/work/kudos-identityprocess.png" />
+		<div class="description-text" style="clear: both;">
+		<p>For the branding, I wanted to create a mark that represents achivement. At first I began with the concept of a present mixed with a heart. But eventually I found the concept to be difficult to understand at first glance, so I moved to the first place ribbon design and refined from there. </p>
+		</div>
+		<ul class="tags">
+			<li>branding</li>
+			<li>ux design</li>
+			<li>illustration</li>
+		</ul>
+	</div>
+</div>
+</div>
+<?php include_once 'sidebar.php'; ?>
+<?php include_once 'bottom.php'; ?>

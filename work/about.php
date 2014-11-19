01314814206cd479f9f2542ae40ec16bcd283327
bforks <brittanyforks@gmail.com>
2014-11-19 15:54:00 -0800

    Add resume & new info to /about

--- work/about.php
+++ work/about.php
@@ -2,9 +2,13 @@
 <div class="main-with-sidebar">
 	<div class="content">
 	<img class="portrait" src="../img/cg-brit-lake-420px.gif"/>
-	
-	<h1 class="hello">Hello there.</h1>
-<p>I'm a designer and illustrator in San Francisco, CA. I like drawing cute things, exploring the city, drinking tea, and spending way too much free time on Tumblr. I'm available for full-time and freelance work, contact me at <a href="mailto:&#x68;&#x65;&#x6C;&#x6C;&#x6F;&#x40;&#x62;&#x72;&#x69;&#x74;&#x74;&#x61;&#x6E;&#x79;&#x66;&#x6F;&#x72;&#x6B;&#x73;&#x2E;&#x63;&#x6F;&#x6D;">hi@brittanyforks.com</a>.</p>
+
+  <h1 class="hello">Hello there.</h1> <p>I'm a designer and illustrator in San
+Francisco, CA. I like drawing cute things, exploring the city, drinking oolong
+tea, and spending way too much free time on Twitter. I'm available for full-time
+and freelance work, contact me at <a href="mailto:&#x68;&#x65;&#x6C;&#x6C;&#x6F;
+&#x40;&#x62;&#x72;&#x69;&#x74;&#x74;&#x61;&#x6E;&#x79;&#x66;&#x6F;&#x72;&#x6B;&#
+x73;&#x2E;&#x63;&#x6F;&#x6D;">hi@brittanyforks.com</a> or check out <a href="../img/BrittanyForks_Resume.pdf">my resume</a>.</p>
 
 	<ul class="three-column">
 	<li class="watching">
@@ -16,27 +20,27 @@
 			<li>Skins</li>
 			<li>Community</li>
 		</ol>
-	</li>	
+	</li>
 	<li class="reading">
 		<h1>Reading</h1>
 		<ol>
 			<li>Steal Like an Artist</li>
 			<li>Rework</li>
-			<li>Designing for Emotion</li>
+			<li>Design is a Job</li>
 			<li>The New Yorker</li>
 			<li>Quora</li>
 		</ol>
-	</li>	
+	</li>
 	<li class="listening">
 		<h1>Listening</h1>
 		<ol>
 			<li>Let's Make Mistakes</li>
 			<li>Purity Ring - Shrines</li>
-			<li>Beyoncé - 4</li>
-			<li>Ellie Goulding - Halcyon</li>
-			<li>ZEDD - Clarity</li>
+			<li>Beyoncé - Beyoncé</li>
+			<li>Lil Wayne - Tha Carter 3</li>
+			<li>Porter Robinson - Worlds</li>
 		</ol>
-	</li>		
+	</li>
 	</ul>
 	</div>
 </div>

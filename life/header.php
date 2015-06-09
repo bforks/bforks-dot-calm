4b38441e4e2f8fc80f4d5eeb90e8db82e93c91b4
brittany forks <brittanyforks@gmail.com>
2015-06-08 21:39:44 -0700

    Renaming Website Title

--- life/header.php
+++ life/header.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Foundation | Welcome</title>
+    <title>Brittany Forks</title>
     <link rel="stylesheet" href="../lib/foundation-essentials-5/css/foundation.css" />
     <link rel="stylesheet" href="css/life.css" />
     <script src="../lib/foundation-essentials-5/js/vendor/modernizr.js"></script>
